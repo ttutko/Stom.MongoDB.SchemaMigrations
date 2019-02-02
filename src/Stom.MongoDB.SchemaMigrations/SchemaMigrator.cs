3a688f3830637149a37238cdd4e5a331c40618d1
Tom Tutko <ttutko@outlook.com>
2019-02-02 13:07:05 -0500

    Added SchemaVersion class Added SchemaMigrator class Created xUnit test project

---
+++ src/Stom.MongoDB.SchemaMigrations/SchemaMigrator.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stom.MongoDB.SchemaMigrations
+{
+    public class SchemaMigrator
+    {
+        private readonly IMongoDatabase db;
+
+        public SchemaMigrator(IMongoDatabase db)
+        {
+            this.db = db;
+        }
+    }
+}
---
+++ src/Stom.MongoDB.SchemaMigrations/SchemaVersion.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stom.MongoDB.SchemaMigrations
+{
+    public abstract class SchemaVersion
+    {
+        private Version version;
+
+        public SchemaVersion(string version)
+        {
+            this.version = Version.Parse(version);
+        }
+
+        public abstract bool Apply(IMongoDatabase db);
+    }
+}
